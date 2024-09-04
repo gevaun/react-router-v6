cd5efa35fc95dec3fccabb378865e57660d48427
Gevaun <geniepinguin@gmail.com>
2024-09-04 18:18:11 -0500

    chore: Update VanElement component to dynamically set background color for type label

--- src/components/VanElement.jsx
+++ src/components/VanElement.jsx
@@ -1,7 +1,21 @@
-
 export default function VanRental(props) {
 
     console.log(props)
+    
+    const getColorByType = (type) => {
+        switch (type) {
+            case 'simple':
+                return 'orange';
+            case 'rugged':
+                return 'gray';
+            case 'luxury':
+                return 'blue';
+            default:
+                return 'gray';
+        }
+    };
+
+    const colorClass = getColorByType(props.item.type);
 
     return (
         <div className="w-96 space-y-4">
@@ -13,7 +27,7 @@ export default function VanRental(props) {
                 <h2 className="text-lg">
                     <span className="font-medium">${props.item.price}</span>/day
                 </h2>
-                <p className="py-2.5 px-6 text-sm bg-indigo-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-500 hover:bg-indigo-700">{props.item.type}</p>
+                <p className={`py-2.5 px-6 w-fit text-md bg-${colorClass}-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-${colorClass}-700`}>{props.item.type}</p>
             </div>
         </div>
     )
