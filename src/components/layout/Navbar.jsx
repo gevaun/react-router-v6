792bdad75ff26408ca3078a8a4c5707d1bdf1001
Gevaun <geniepinguin@gmail.com>
2024-09-04 22:07:37 -0500

    chore: Update Navbar component to remove unnecessary CSS class

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-
-
+import { Disclosure } from '@headlessui/react'
 
 export default function Example() {
 
@@ -17,7 +15,7 @@ export default function Example() {
 
     return (
         <Disclosure as="nav" className="flex items-center h-28">
-            <div className='flex w-full px-4'> 
+            <div className='flex w-full px-4'>
                 <div className='w-1/2'>
                     <Link to='/' className='text-2xl font-black'>#VANLIFE</Link>
                 </div>
