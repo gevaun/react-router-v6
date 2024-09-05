534d4db0c23459d5af6d80e245701645a332856f
Gevaun <geniepinguin@gmail.com>
2024-09-04 20:37:55 -0500

    chore: Add color label for van type in VanDetail.jsx

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -6,7 +6,21 @@ export default function VanDetail() {
     const params = useParams();
     const [van, setVan] = useState({});
 
-    console.log(params);
+    const getColorByType = (type) => {
+        switch (type) {
+            case 'simple':
+                return 'orange';
+            case 'rugged':
+                return 'gray';
+            case 'luxury':
+                return 'blue';
+            default:
+                return 'gray';
+        }
+    };
+
+    const colorClass = getColorByType(van.type);
+
 
     useEffect(() => {
         fetch(`/api/vans/${params.id}`)
@@ -18,7 +32,7 @@ export default function VanDetail() {
 
     return (
         <div className="space-y-4">
-            <Link to="/vans" className="flex">
+            <Link to="/vans" className="flex w-fit">
                 <ArrowLeftIcon className="w-4" /><span className="underline underline-offset-4">Back to all vans</span>
             </Link>
             <div className="space-y-4">
@@ -30,6 +44,7 @@ export default function VanDetail() {
                     <h2 className="text-lg">
                         <span className="font-medium">${van.price}</span>/day
                     </h2>
+                    <p className={`py-2.5 px-6 w-fit text-md bg-${colorClass}-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-${colorClass}-700`}>{van.type}</p>
                     <p>{van.description}</p>
                     <p className={`py-2.5 px-6 w-full text-md bg-amber-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-amber-700`}>Rent this van</p>
                 </div>
