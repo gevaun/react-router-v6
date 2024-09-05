5b52ed4855bf08dbc195d0391c3a6204187f1440
Gevaun <geniepinguin@gmail.com>
2024-09-04 20:33:22 -0500

    chore: Add react-router-dom Link to VanElement component for improved navigation

--- src/components/VanElement.jsx
+++ src/components/VanElement.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function VanRental(props) {
 
     console.log(props)
-    
+
     const getColorByType = (type) => {
         switch (type) {
             case 'simple':
@@ -19,16 +21,19 @@ export default function VanRental(props) {
 
     return (
         <div className="w-96 space-y-4">
-            <div className="">
-                <img src={props.item.imageUrl} alt={props.item.name} className="" />
-            </div>
-            <div className="space-y-2">
-                <h1 className="text-2xl font-medium">{props.item.name}</h1>
-                <h2 className="text-lg">
-                    <span className="font-medium">${props.item.price}</span>/day
-                </h2>
-                <p className={`py-2.5 px-6 w-fit text-md bg-${colorClass}-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-${colorClass}-700`}>{props.item.type}</p>
-            </div>
+            <Link to={`/vans/${props.item.id}`} aria-label={`View details for ${props.item.name}, 
+                             priced at $${props.item.price} per day`}>
+                <div className="">
+                    <img src={props.item.imageUrl} alt={`Image of ${props.item.name}`} className="" />
+                </div>
+                <div className="space-y-2">
+                    <h1 className="text-2xl font-medium">{props.item.name}</h1>
+                    <h2 className="text-lg">
+                        <span className="font-medium">${props.item.price}</span>/day
+                    </h2>
+                    <p className={`py-2.5 px-6 w-fit text-md bg-${colorClass}-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-${colorClass}-700`}>{props.item.type}</p>
+                </div>
+            </Link>
         </div>
     )
 }
