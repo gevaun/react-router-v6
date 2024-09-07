7e6b2d2a599529d5bb29a355d10a2abf3dafcc6b
Gevaun <geniepinguin@gmail.com>
2024-09-06 20:49:26 -0500

    refactor: Add Footer component to Layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="mt-auto py-3 bg-gray-800 dark:bg-neutral-800">
+            <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8f flex justify-center">
+                <p className="text-white">©️ 2024 #VANLIFE</p>
+            </div>
+        </footer>
+    )
+}
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,15 @@
-
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
+import Footer from "../Footer"
 
 export default function Layout() {
     return (
-        <div>
+        <div className="bg-amber-50 dark:bg-zinc-900 dark:text-white min-h-screen flex flex-col">
             <Navbar />
-            <div className="container mx-auto max-w-7xl px-4 py-6">
+            <div className="container mx-auto max-w-7xl px-4 py-6 flex-grow">
                 <Outlet />
             </div>
+            <Footer />
         </div>
-    )
+    );
 }
