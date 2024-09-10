883eb269b7bdcfd1a7f006492aa74341421ed2b5
Gevaun <geniepinguin@gmail.com>
2024-09-10 17:20:17 -0500

    Refactor VanDetail component and add back button to return to previous page

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,54 +1,51 @@
 import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 
 export default function VanDetail() {
-    const params = useParams();
-    const [van, setVan] = useState({});
+  const params = useParams();
+  const [van, setVan] = useState({});
+  const location = useLocation()
+  const search = location.state?.search || "";
+  const type = location.state?.type || "all";
 
-    const getColorByType = (type) => {
-        switch (type) {
-            case 'simple':
-                return 'orange';
-            case 'rugged':
-                return 'gray';
-            case 'luxury':
-                return 'blue';
-            default:
-                return 'gray';
-        }
-    };
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
 
-    const colorClass = getColorByType(van.type);
-
-
-    useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-            .then(response => response.json())
-            .then(data => setVan(data.vans));
-    }, [params.id]);
-
-    console.log(van);
-
-    return (
-        <div className="space-y-4">
-            <Link to="/vans" className="flex w-fit">
-                <ArrowLeftIcon className="w-4" /><span className="underline underline-offset-4">Back to all vans</span>
-            </Link>
-            <div className="space-y-4">
-                <div className="">
-                    <img src={van.imageUrl} alt={`Image of ${van.name}`} className="" />
-                </div>
-                <div className="space-y-2">
-                    <h1 className="text-2xl font-medium">{van.name}</h1>
-                    <h2 className="text-lg">
-                        <span className="font-medium">${van.price}</span>/day
-                    </h2>
-                    <p className={`py-2.5 px-6 w-fit text-md bg-${colorClass}-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-${colorClass}-700`}>{van.type}</p>
-                    <p>{van.description}</p>
-                    <p className={`py-2.5 px-6 w-full text-md bg-amber-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-amber-700`}>Rent this van</p>
-                </div>
-            </div>
+  return (
+    <div className="space-y-4">
+      <Link 
+        to={`..${search}`}
+        relative="path"
+        className="flex w-fit">
+        <ArrowLeftIcon className="w-4" />
+        <span className="underline underline-offset-4">{`Back to ${type} vans`}</span>
+      </Link>
+      <div className="space-y-4">
+        <div className="">
+          <img src={van.imageUrl} alt={`Image of ${van.name}`} className="" />
         </div>
-    );
+        <div className="space-y-2">
+          <h1 className="text-2xl font-medium">{van.name}</h1>
+          <h2 className="text-lg">
+            <span className="font-medium">${van.price}</span>/day
+          </h2>
+          <p
+            className={`py-2.5 px-6 w-fit text-md bg-amber-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-amber-700`}
+          >
+            {van.type}
+          </p>
+          <p>{van.description}</p>
+          <p
+            className={`py-2.5 px-6 w-full text-md bg-amber-500 text-white rounded-lg cursor-pointer font-semibold text-center shadow-xs transition-all duration-300 hover:bg-amber-700`}
+          >
+            Rent this van
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
