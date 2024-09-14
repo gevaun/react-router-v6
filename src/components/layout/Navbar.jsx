1f4ddfd34c53de6787c40c0f0e97858fe4510984
gevaun <geniepinguin@gmail.com>
2024-09-13 22:26:31 -0500

    feat: Add login page and login icon to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,34 +1,47 @@
-import { NavLink } from 'react-router-dom'
-import { Disclosure } from '@headlessui/react'
+import { NavLink } from "react-router-dom";
+import { Disclosure } from "@headlessui/react";
+import { UserCircleIcon } from "@heroicons/react/16/solid";
 
 export default function Example() {
+  const navItems = [
+    { name: "Host", link: "/host" },
+    { name: "About", link: "/about" },
+    { name: "Vans", link: "/vans" },
+  ];
 
-    const navItems = [
-        { name: 'Host', link: '/host' },
-        { name: 'About', link: '/about' },
-        { name: 'Vans', link: '/vans' }
-    ]
+  const navItemElements = navItems.map((item, index) => (
+    <NavLink
+      key={index}
+      to={item.link}
+      className={({ isActive }) =>
+        isActive
+          ? "text-2xl font-medium underline underline-offset-4"
+          : "text-2xl font-medium opacity-75 transition-all duration-200 hover:opacity-100"
+      }
+    >
+      {item.name}
+    </NavLink>
+  ));
 
-    const navItemElements = navItems.map((item, index) => (
-        <NavLink
-            key={index}
-            to={item.link}
-            className={({ isActive }) => isActive ? 'text-2xl font-medium underline underline-offset-4' : 'text-2xl font-medium opacity-75 transition-all duration-200 hover:opacity-100'}
-        >
-            {item.name}
-        </NavLink>
-    ))
-
-    return (
-        <Disclosure as="nav" className="flex items-center h-28">
-            <div className='flex w-full px-4'>
-                <div className='w-1/2'>
-                    <NavLink to='/' className='text-2xl font-black'>#VANLIFE</NavLink>
-                </div>
-                <div className='w-1/2 flex justify-end space-x-10'>
-                    {navItemElements}
-                </div>
-            </div>
-        </Disclosure>
-    )
+  return (
+    <Disclosure as="nav" className="flex items-center h-28">
+      <div className="flex w-full px-4">
+        <div className="w-1/2">
+          <NavLink to="/" className="text-2xl font-black">
+            #VANLIFE
+          </NavLink>
+        </div>
+        <div className="w-1/2 flex justify-end space-x-10">
+          {navItemElements}
+          <NavLink
+            to="login"
+            className="self-center opacity-75 hover:opacity-100 transition-all duration-200"
+            title="Login"
+          >
+            <UserCircleIcon className="w-6" />
+          </NavLink>
+        </div>
+      </div>
+    </Disclosure>
+  );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import { useState, useId } from "react";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  const id = useId();
+
+  return (
+    <div className="flex flex-col items-center h-screen ">
+      <h1 className="text-3xl font-bold">Sign in to your account</h1>
+      <div>
+        <form className="flex flex-col space-y-4 mt-4" onSubmit={handleSubmit}>
+          <label htmlFor="{`${id}-email`}">Email</label>
+          <input
+            type="email"
+            placeholder="Email Address"
+            id={`${id}-email`}
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="border border-gray-300 rounded-md px-2 py-1"
+          />
+          <label htmlFor="{`${id}-password`}">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            id={`${id}-password`}
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="border border-gray-300 rounded-md px-2 py-1"
+          />
+          <button
+            type="submit"
+            className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+      <div className="p-4">
+        Do not have an account?
+        <Link to="" className="font-medium text-orange-600"> Create one now</Link>
+      </div>
+    </div>
+  );
+}
