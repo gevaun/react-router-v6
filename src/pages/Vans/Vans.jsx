c4f39907db377d7ca9423d69843dccfbb412605b
Gevaun <geniepinguin@gmail.com>
2024-09-22 16:05:23 -0500

    Refactor Vans component to use deferred loading and add filter functionality

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
-import { NavLink, useSearchParams, useLoaderData } from "react-router-dom";
+import {
+  NavLink,
+  useSearchParams,
+  useLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import VanRental from "../../components/van/VanElement";
 import { getVans } from "../../utils/api";
 
 import "../../server";
 
 export function loader() {
-    return getVans();
-} 
+  return defer({ vans: getVans() });
+}
 
 export default function Vans() {
   let [searchParams, setSearchParams] = useSearchParams();
@@ -15,66 +21,72 @@ export default function Vans() {
   // Handle error state
   const [error, setError] = useState(null);
   // Extract the data from the loader
-  const vans = useLoaderData();
-
-  // Extract unique van types
-  const uniqueVanTypes = [...new Set(vans.map((van) => van.type))];
-
-  // Map over unique van types to create button elements
-  const vanTypesButtonElements = uniqueVanTypes.map((type) => (
-    <button
-      onClick={() => handleFilterChange("type", type)}
-      key={type}
-      className={`py-2.5 px-6 text-sm bg-amber-300 text-slate-700 rounded-lg font-semibold text-center ${
-        typeFilter === type ? "bg-amber-500" : "hover:bg-amber-500"
-      }`}
-    >
-      {type.charAt(0).toUpperCase() + type.slice(1)}
-    </button>
-  ));
-
-  function handleFilterChange(key, value) {
-    setSearchParams((prevParams) => {
-      if (value === null) {
-        prevParams.delete(key);
-      } else {
-        prevParams.set(key, value);
-      }
-      return prevParams;
-    });
-  }
-
-  // map over the vans array and render a VanRental component for each van
-  // pass the van object as a prop to the VanRental component
-  const filteredVans = typeFilter
-    ? vans.filter((van) => van.type === typeFilter)
-    : vans;
-
-  const vanElements = filteredVans.map((van) => (
-    <VanRental key={van.id} item={van} />
-  ));
+  // const vans = useLoaderData();
+  const dataPromise = useLoaderData();
 
   // handle error state
   if (error) {
-    return <h1 aria-live="assertive">There was an error: {error.message}</h1>
-}
+    return <h1 aria-live="assertive">There was an error: {error.message}</h1>;
+  }
 
   return (
     <div>
       <div className="">
         <h1 className="text-4xl font-bold pb-4">Explore our van options</h1>
       </div>
-      <div className="space-x-10 py-8">
-        {vanTypesButtonElements}
-        {typeFilter && (
-          <NavLink to="." className="opacity-75 hover:opacity-100">
-            Clear Filters
-          </NavLink>
-        )}
-      </div>
-      <div className=" grid grid-cols-2 gap-6 flex-wrap justify-center">
-        {vanElements}
-      </div>
+      <Await resolve={dataPromise.vans}>
+        {(vans) => {
+          // map over the vans array and render a VanRental component for each van
+          // pass the van object as a prop to the VanRental component
+          const filteredVans = typeFilter
+            ? vans.filter((van) => van.type === typeFilter)
+            : vans;
+
+          const vanElements = filteredVans.map((van) => (
+            <VanRental key={van.id} item={van} />
+          ));
+          // Extract unique van types
+          const uniqueVanTypes = [...new Set(vans.map((van) => van.type))];
+
+          // Map over unique van types to create button elements
+          const vanTypesButtonElements = uniqueVanTypes.map((type) => (
+            <button
+              onClick={() => handleFilterChange("type", type)}
+              key={type}
+              className={`py-2.5 px-6 text-sm bg-amber-300 text-slate-700 rounded-lg font-semibold text-center ${
+                typeFilter === type ? "bg-amber-500" : "hover:bg-amber-500"
+              }`}
+            >
+              {type.charAt(0).toUpperCase() + type.slice(1)}
+            </button>
+          ));
+          function handleFilterChange(key, value) {
+            setSearchParams((prevParams) => {
+              if (value === null) {
+                prevParams.delete(key);
+              } else {
+                prevParams.set(key, value);
+              }
+              return prevParams;
+            });
+          }
+          return (
+            <>
+              <div className="space-x-10 py-8">
+                {vanTypesButtonElements}
+                {typeFilter && (
+                  <NavLink to="." className="opacity-75 hover:opacity-100">
+                    Clear Filters
+                  </NavLink>
+                )}
+              </div>
+              <div className=" grid grid-cols-2 gap-6 flex-wrap justify-center">
+                {vanElements}
+              </div>
+            </>
+          );
+        }}
+      </Await>
     </div>
   );
 }
