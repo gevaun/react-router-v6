2dc0ada311fc1e01b58b303fe638da95dfdb6534
Gevaun <geniepinguin@gmail.com>
2024-09-21 21:44:09 -0500

    Refactor imports in utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-import { redirect, Navigate } from "react-router-dom";
+import { redirect } from "react-router-dom";
 
 export async function requireAuth(request) {
   const pathname = new URL(request.url).pathname;
