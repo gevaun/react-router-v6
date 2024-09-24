d44adbb451249f01b2ff770888dc2e00b020ce31
Gevaun <geniepinguin@gmail.com>
2024-09-23 22:05:10 -0500

    Refactor Vans component to use Suspense for deferred loading and add filter functionality

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,26 +1,40 @@
-import { useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, defer, Await } from "react-router-dom";
 import { getHostVans } from "../../utils/api";
 import { requireAuth } from "../../utils/utils";
 import Card from "../../components/host/Card";
+import { render } from "react-dom";
 
 export async function loader({ request }) {
     await requireAuth(request);
-    return getHostVans();
+    return defer({ vans: getHostVans() });
 }
 
 export default function Vans() {
   // Use the useLoaderData hook to get the data that was loaded by the loader
-  const vans = useLoaderData();
+  const dataPromise = useLoaderData();
 
   // Create a new array of Card components using the data that was loaded
-  const vanElement = vans.map((van, index) => <Card key={index} item={van} />);
+  function renderVanElements(vans) {
+    const vanElements = vans.map((van) => <Card key={van.id} item={van} />);
+
+    return (
+      <div className="space-y-4">{vanElements}</div>
+    )
+  }
+  // const vanElement = vans.map((van, index) => <Card key={index} item={van} />);
 
   return (
     <div>
       <div className="py-12">
         <h1 className="text-4xl font-bold">Your listed vans</h1>
       </div>
-      <div className="space-y-4">{vanElement}</div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Await resolve={dataPromise.vans}>
+          {renderVanElements}
+        </Await>
+      </Suspense>
+      
     </div>
   );
 }
